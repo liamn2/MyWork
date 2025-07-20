c30b0519e1ce00d4d9b8fc00e1f3786967b693ab
Liam Nutley <evildeadfan123@gmail.com>
2025-07-20 23:37:29 +0100

    Update sparkhadoop.py
    Removed blank lines.

--- sparkhadoop.py
+++ sparkhadoop.py
@@ -1,18 +1,13 @@
 from pyspark.sql import SparkSession
-
 spark = SparkSession.builder \
     .appName("PySparkHadoopExample") \
     .getOrCreate()
-
 # Read data from HDFS
 data = spark.read.text("hdfs://localhost:9000/user/hadoop/input_data")
-
 # Perform word count
 word_counts = data.rdd.flatMap(lambda line: line.split(" ")) \
     .map(lambda word: (word, 1)) \
     .reduceByKey(lambda a, b: a + b)
-
 # Save results to HDFS
 word_counts.saveAsTextFile("hdfs://localhost:9000/user/hadoop/output_data")
-
 spark.stop()
